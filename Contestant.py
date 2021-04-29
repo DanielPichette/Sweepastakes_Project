8aeac6ce71ddee2d7aed03555710c3ca62bf1c82
DanielPichette <daniel.f.pichette@gmail.com>
2021-04-29 09:58:09 -0500

    completed contestant class within Contestant.py

--- Contestant.py
+++ Contestant.py
@@ -8,3 +8,9 @@ class Contestant:
 
     def notify(self):
         print('has been notified')
+
+    def create_contestant(self):
+        self.first_name: input('first name: ')
+        self.last_name: input('last name: ')
+        self.email_address: input('email address: ')
+        self.registration_number: input('registration number: ')
