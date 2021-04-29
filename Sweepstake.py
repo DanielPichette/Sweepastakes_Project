2efde4174ff4a6198ad0ceebc2aec65fe69fe8d7
DanielPichette <daniel.f.pichette@gmail.com>
2021-04-29 11:12:07 -0500

    completed sweepstakes class within Sweepstake.py

--- Sweepstake.py
+++ Sweepstake.py
@@ -1,17 +1,21 @@
+import random
+
 from Contestant import Contestant
 
 
 class Sweepstake:
     def __init__(self):
-        self.contestants: []
-        self.name: None
+        self.contestants: {}
+        self.name: input('what is the name of this sweepstakes?')
 
     # member methods
     def register_contestant(self, contestant):
-        pass
+        self.contestants[contestant.registration_number] = (contestant.first_name + ' ' + contestant.last_name)
 
     def pick_winner(self):
-        pass
+        list_of_contestants = list(self.contestants.items())
+        winner = random.choice(list_of_contestants)
+        return winner
 
     def print_contestant_info(self, contestant):
-        pass
+        print(contestant)
